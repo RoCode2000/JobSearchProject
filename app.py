2ca2c391734446877efb58918a760a0deb6ae302
limwei1 <115162049+limwei1@users.noreply.github.com>
2023-11-26 14:15:00 +0000

    2

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import csv
+import datetime
+import pytz
+import requests
+import subprocess
+import urllib
+import uuid
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+##db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+def index():
+    return render_template(index.html)
